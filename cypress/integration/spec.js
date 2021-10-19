c90a749f3c461bcb38d4b4cb38b5de93e9ccfc19
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-19 08:09:37 -0400

    use the response timestamp

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,15 +3,31 @@
 import('../..')
 
 it('waits for the network call', () => {
-  cy.visit('cypress/index.html')
+  cy.visit('/')
 
   cy.waitForNetworkIdle(2000)
     .should('have.keys', 'started', 'finished', 'waited', 'callCount')
     .then(({ waited, callCount }) => {
-      // the page makes the second Ajax call after 1500 ms
-      // thus total resolve time should be >= 3500 ms
+      // the page makes the Ajax call after 1000 ms
+      // thus total resolve time should be >= 3000 ms
       // but probably under 4 seconds
-      expect(waited, 'waited ms').to.be.within(3500, 4000)
-      expect(callCount, 'callCount').to.equal(2)
+      expect(waited, 'waited ms').to.be.within(3000, 4000)
+      expect(callCount, 'callCount').to.equal(1)
+    })
+})
+
+it('uses the response timestamp', () => {
+  cy.visit('/delayed')
+
+  cy.waitForNetworkIdle('GET', '/user/delayed', 3000)
+    .should('have.keys', 'started', 'finished', 'waited', 'callCount')
+    .then(({ waited, callCount }) => {
+      // the page makes an Ajax call after 1500 ms
+      // which is delayed by 1000 ms on the server
+      // thus total resolve time should be:
+      // 1000 + 1000 + 3000
+      // but probably under 7 seconds
+      expect(waited, 'waited ms').to.be.within(5000, 7000)
+      expect(callCount, 'callCount').to.equal(1)
     })
 })
