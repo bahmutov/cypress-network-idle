3848412af0b589799a28a9d2d6b76427ed98111f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-19 10:00:39 -0400

    refactoring into counters object

--- cypress/integration/button.js
+++ cypress/integration/button.js
@@ -4,10 +4,15 @@ import('../../src')
 
 it('starts listening before', () => {
   cy.visit('/button')
+  cy.waitForNetworkIdlePrepare({
+    method: 'GET',
+    pattern: '/user',
+    alias: 'user',
+  })
   cy.get('#fetch').click()
-  cy.waitForNetworkIdle('/user', 1000)
-    .should('have.keys', 'started', 'finished', 'waited', 'callCount')
-    .then(({ waited, callCount }) => {
-      expect(callCount, 'callCount').to.equal(1)
-    })
+  // cy.waitForNetworkIdle('/user', 1000)
+  //   .should('have.keys', 'started', 'finished', 'waited', 'callCount')
+  //   .then(({ waited, callCount }) => {
+  //     expect(callCount, 'callCount').to.equal(1)
+  //   })
 })
