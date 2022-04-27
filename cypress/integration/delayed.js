8e1f95fcfc644c8c075f4c3183297ed5e435bdc7
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2022-04-27 14:50:11 -0600

    update command typings

--- cypress/integration/delayed.js
+++ cypress/integration/delayed.js
@@ -18,6 +18,22 @@ it('uses the response timestamp', () => {
     })
 })
 
+it('works with options object', () => {
+  cy.visit('/delayed')
+
+  cy.waitForNetworkIdle(3000, { method: 'GET', pattern: '/user/delayed' })
+    .should('have.keys', 'started', 'finished', 'waited', 'callCount')
+    .then(({ waited, callCount }) => {
+      // the page makes an Ajax call after 1500 ms
+      // which is delayed by 1000 ms on the server
+      // thus total resolve time should be:
+      // 1000 + 1000 + 3000
+      // but probably under 7 seconds
+      expect(waited, 'waited ms').to.be.within(5000, 7000)
+      expect(callCount, 'callCount').to.equal(1)
+    })
+})
+
 it('fails when exceeding timeout', () => {
   const message = 'Timed out retrying after 4000ms: Network is busy'
   const failed = `Expected to fail with "${message}"`
